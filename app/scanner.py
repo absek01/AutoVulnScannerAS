2c346c93c712fce7f6922a6db57afbc456f6bac6
absek01 <a_shrestha@mail.fhsu.edu>
2024-12-06 16:47:02 -0500

    Removed zap and added VirusTotal API. ALso made changes to the user interface

--- app/scanner.py
+++ app/scanner.py
@@ -1,59 +1,18 @@
 import requests
-import subprocess
-import platform
-import time
+import base64
 
-
-def start_zap():
-    system = platform.system()
-    if system == 'Windows':
-        zap_path = 'zap/ZAP.exe'
-    else:
-        zap_path = 'zap/zap.sh'
-
-    try:
-        subprocess.Popen([zap_path, '-daemon'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        time.sleep(10)  # Wait for ZAP to start
-    except Exception as e:
-        print(f"Error starting ZAP: {e}")
-
-
-ZAP_URL = 'http://localhost:8080'  # URL where ZAP API is available
-
+API_KEY = '23bad8c89f9b88355eccedf72a47b349d3e722eeee22eb23d417e579a9a68c49'
+VT_URL = 'https://www.virustotal.com/api/v3/urls/'
 
 def scan_url(url):
-    start_zap()  # Ensure ZAP is running
-    start_scan(url)
-    results = {
-        'sql_injection': check_sql_injection(),
-        'xss': check_xss()
+    url_id = get_url_id(url)
+    headers = {
+        'x-apikey': API_KEY,
+        'Accept': 'application/json'
     }
-    return results
-
-
-def start_scan(url):
-    scan_url = f'{ZAP_URL}/JSON/ascan/action/scan/?url={url}'
-    response = requests.get(scan_url)
+    response = requests.get(f'{VT_URL}{url_id}', headers=headers)
     return response.json()
 
-
-def check_sql_injection():
-    issues = get_alerts()
-    for issue in issues:
-        if 'SQL Injection' in issue['alert']:
-            return True
-    return False
-
-
-def check_xss():
-    issues = get_alerts()
-    for issue in issues:
-        if 'Cross Site Scripting' in issue['alert']:
-            return True
-    return False
-
-
-def get_alerts():
-    alerts_url = f'{ZAP_URL}/JSON/core/view/alerts/'
-    response = requests.get(alerts_url)
-    return response.json().get('alerts', [])
+def get_url_id(url):
+    url_id = base64.urlsafe_b64encode(url.encode()).decode().strip('=')
+    return url_id
