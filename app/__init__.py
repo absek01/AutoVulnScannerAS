bfd7cddc452f642a49181f40a903a21f7ef55f3e
absek01 <a_shrestha@mail.fhsu.edu>
2024-12-06 16:12:49 -0500

    made changes to html and init file. Also imported ZAP

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 import os
 
 def create_app():
-    app = Flask(__name__, template_folder=os.path.abspath('templates'))
+    app = Flask(__name__, template_folder=os.path.abspath('templates'), static_folder=os.path.abspath('static'))
     app.config.from_object('config')
 
     from .routes import main as main_blueprint
